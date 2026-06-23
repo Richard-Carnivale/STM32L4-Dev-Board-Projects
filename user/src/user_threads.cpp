3a13da86801e90e4fbb50bafbb439f549b75406a
Richard-Carnivale <145724604+Richard-Carnivale@users.noreply.github.com>
2026-06-23 08:48:06 -0400

    defining timer and starting it

--- user/src/user_threads.cpp
+++ user/src/user_threads.cpp
@@ -9,12 +9,19 @@
 
 #include "main.h"
 
+extern TIM_HandleTypeDef htim1;
+
 extern "C" {
     void StartDefaultTask_user(void const * argument)
     {
+        HAL_TIM_Base_Start_IT(&htim1);
+        
         for (;;)
         {
             HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+            if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin)) {
+
+            }
             osDelay(200);
         }
     }
